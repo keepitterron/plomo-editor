4db5c51b26c397cedb8b85ba19cd821341aade2e
Claudio Semeraro <raptxt@gmail.com>
2018-05-29 22:57:42 +0200

    chore: fix for local development

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import PlomoEditor from './components/PlomoEditor';
 import './app.css';
 
-if(process && process.env.NODE_ENV) {
+if(IS_PLOMO_EDITOR_DEVELOPMENT) {
   ReactDOM.render(<PlomoEditor />, document.getElementById('app'));
 }
 
